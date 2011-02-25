b0f363cba10eb68c88aff8026029d3a15a6b07f1
Josh Marcus <josh.marcus@gmail.com>
2011-02-25 17:14:57 -0500

    change ID to an integer

--- phillyleg/models.py
+++ phillyleg/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Subscription(models.Model):
 #   user   = models.ForeignKey(User, unique=True)
     email  = models.CharField(max_length=100)
-    lastId = models.IntField()
+    lastId = models.IntegerField()
 
     def __unicode__(self):
         return self.email
@@ -28,7 +28,7 @@ class LegFile(models.Model):
     date_scraped = models.CharField(max_length=1000)
     final_date = models.CharField(max_length=1000)
     intro_date = models.CharField(max_length=1000)
-    key = models.CharField(max_length=1000)
+    key = models.IntegerField()
     sponsors = models.CharField(max_length=1000)
     status = models.CharField(max_length=1000)
     title = models.CharField(max_length=1000)
