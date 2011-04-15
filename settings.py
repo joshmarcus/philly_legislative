1dc9e640d4432b4b9e4785711564cdb1b32e45a4
Mjumbe Wawatu Poe <mjumbe@codeforamerica.org>
2011-04-15 12:47:23 -0700

    Make path to database in settings file absolute

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'philly_leg.sqlite3'             # Or path to database file if using sqlite3.
+DATABASE_NAME = rel_path('philly_leg.sqlite3')             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
