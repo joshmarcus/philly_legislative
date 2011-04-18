ea69dcffc81627710dab8007513d9f9125b7b63d
Mjumbe Wawatu Poe <mjumbe@codeforamerica.org>
2011-04-18 12:12:58 -0700

    Change the reference to KeywordSubscription in the admin module

--- phillyleg/admin.py
+++ phillyleg/admin.py
@@ -3,7 +3,7 @@ from phillyleg.models import Subscription
 from phillyleg.models import *
 
 class KeywordInline(admin.StackedInline):
-    model = Keyword
+    model = KeywordSubscription
     extra = 3
 
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -11,6 +11,6 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Subscription, SubscriptionAdmin)
-admin.site.register(Keyword)
+admin.site.register(KeywordSubscription)
 admin.site.register(LegFile)
 admin.site.register(CouncilMember)
